364101888710f0ea9b603b67f87f7bde3535c1b4
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-02-06 21:03:24 +0300

    Shop menu and logic

---
+++ components/main/main.js
@@ -0,0 +1,21 @@
+document.getElementById("first-categories").onclick = () => {
+    document.getElementById("first-block").style.display = "flex"
+    document.getElementById("second-block").style.display = "none"
+    document.getElementById("third-block").style.display = "none"
+};
+document.getElementById("second-categories").onclick = () => {
+    document.getElementById("first-block").style.display = "none"
+    document.getElementById("second-block").style.display = "flex"
+    document.getElementById("third-block").style.display = "none"
+};
+document.getElementById("third-categories").onclick = () => {
+    document.getElementById("first-block").style.display = "none"
+    document.getElementById("second-block").style.display = "none"
+    document.getElementById("third-block").style.display = "flex"
+};
+document.getElementById("peach").onclick = () =>{
+    document.getElementById("peach").style.animationName = "shake"
+}
+
+
+
