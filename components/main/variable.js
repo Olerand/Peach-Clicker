f38c9eb5d241652d2e19adfaab05065621c699ed
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-02-14 22:13:02 +0300

    ...

--- components/main/variable.js
+++ components/main/variable.js
@@ -1,9 +1,13 @@
-// localStorage.click_cost = 0.1
-//  localStorage.money = 0
+
 localStorage.money = Number(localStorage.money) || 0;
 localStorage.click_cost = Number(localStorage.click_cost) || 1;
 localStorage.passive_income_per_second = Number(localStorage.passive_income_per_second) || 0;
-console.log(exitTime)
-window.onbeforeunload = function() {
-    alert(1);
-  };
+localStorage.coef_click = Number(localStorage.localStorage.coef_click) || 1;
+localStorage.fill_bar = false
+function resetSettings(){
+  localStorage.click_cost = 0.1
+  localStorage.money = 0
+  localStorage.passive_income_per_second = 0
+  localStorage.coef_click  = 1
+}
+resetSettings()
